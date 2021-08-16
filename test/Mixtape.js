408da093699cceddf0097e1b53bfecb976dd6256
Michael Gingras <mcg79@cornell.edu>
2021-08-15 21:47:48 -0400

    bidding test cases are passing

--- test/Mixtape.js
+++ test/Mixtape.js
@@ -80,41 +80,51 @@ describe("Mixtape contract", function () {
 
 
   describe("Bidding on an NFT", function () {
-      // setup bid so it is available
-      // beforeEach(async function() {
-      //     const _testURI = "this should pass";
-      //     const tx = await mixtapeContract.createMixtape(owner.address, _testURI, 1, 2, 3);
-      //     const bidtx = await mixtapeContract.connect(addr1).bidForCassette(0, {value: ethers.utils.parseEther("10.0")});
-      // });
+      // 1. deploying the contract with a small demo tree
+      // 2. claiming a tape, so it is available to make bids against in future test cases
+      before('deploy', async () => {
+        tree = new TapeTree([
+          { capacity: BigNumber.from(10), quality: BigNumber.from(10), style: BigNumber.from(10) },
+          { capacity: BigNumber.from(10), quality: BigNumber.from(10), style: BigNumber.from(10) }
+        ])
 
-      // it("Should place a bid successfully", async function () {
-      //     const bid = await mixtapeContract.cassetteBids(0);
+        mixtapeContract = await Mixtape.deploy(tree.getHexRoot())
+        await mixtapeContract.deployed();
 
-      //     expect(bid.bidder === addr1.address);
-      //     expect(bid.cassetteIndex === 0);
-      //     expect(bid.amount === ethers.utils.parseEther("1.0"));
-      // });
+        const _testURI = "test of some metadata";
+        const proof0 = tree.getProof(0, BigNumber.from(10), BigNumber.from(10), BigNumber.from(10));
+        await mixtapeContract.claim(0, BigNumber.from(10), BigNumber.from(10), BigNumber.from(10), proof0, _testURI)
+      })
 
-      // it("Should accept bid successfully", async function () {
-      //     const balanceBefore = await ethers.provider.getBalance(owner.address);
-      //     const atx = await mixtapeContract.acceptBidForCassette(0);
 
-      //     // should clear out bid
-      //     const bid = await mixtapeContract.cassetteBids(0);
-      //     expect(bid.bidder === 0x0);
-      //     expect(bid.cassetteIndex === 0);
-      //     expect(bid.amount === ethers.utils.parseEther("0.0"));
+      it("Should place a bid successfully", async function () {
+          const bidtx = await mixtapeContract.connect(addr1).bid(0, {value: ethers.utils.parseEther("10.0")});
 
-      //     // should transfer ownership
-      //     const newOwner = await mixtapeContract.ownerOf(0);
-      //     expect(newOwner === addr1.address);
+          const bid = await mixtapeContract.cassetteBids(0);
+          expect(bid.bidder === addr1.address);
+          expect(bid.cassetteIndex === 0);
+          expect(bid.amount === ethers.utils.parseEther("1.0"));
+      });
 
-      //     // should pay previous owner
-      //     // the bid is for 10 eth, but it costs gas to accept bid so we cant check
-      //     // that the profit is exactly 10.
-      //     const balanceAfter = await ethers.provider.getBalance(owner.address);
-      //     expect(ethers.utils.formatEther(balanceAfter) - ethers.utils.formatEther(balanceBefore) > 9.99)
-      // })
+      it("Should accept bid successfully", async function () {
+          const balanceBefore = await ethers.provider.getBalance(owner.address);
+          const atx = await mixtapeContract.acceptBid(0);
 
+          // should clear out bid
+          const bid = await mixtapeContract.cassetteBids(0);
+          expect(bid.bidder === 0x0);
+          expect(bid.cassetteIndex === 0);
+          expect(bid.amount === ethers.utils.parseEther("0.0"));
+
+          // should transfer ownership
+          const newOwner = await mixtapeContract.ownerOf(0);
+          expect(newOwner === addr1.address);
+
+          // should pay previous owner
+          // the bid is for 10 eth, but it costs gas to accept bid so we cant check
+          // that the profit is exactly 10.
+          const balanceAfter = await ethers.provider.getBalance(owner.address);
+          expect(ethers.utils.formatEther(balanceAfter) - ethers.utils.formatEther(balanceBefore) > 9.99)
+      })
   })
 });
